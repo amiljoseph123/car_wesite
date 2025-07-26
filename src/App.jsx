bd071091282886374494409765c666338096b2c6
amiljoseph123 <amiljoseph2024a@mca.ajce.in>
2025-07-26 23:28:51 +0530

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { useState } from "react";
+import Background from './components/Background/Background.JSX'; 
+
+
 
 const App = () => {
 
+ 
+
   let heroData = [
-    {text1: "Dive into", text2: " What you love"}
-  ]
+  { text1: "Dive into", text2: " What you love" },
+  { text1: "Dive into", text2: " What you love" },
+  { text1: "Dive into", text2: " What you love" }
+]
+
+  const [heroCount,setHeroCount] = useState(0);
+  
+  const [playStatus, setPlayStatus] = useState(false);
+
   return (
     <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+
       
     </div>
   )
 }
 
 export default App
+
+
+
--- src/components/background/background.jsx
+++ src/components/background/background.jsx
@@ -1,11 +1,25 @@
-import './background.css'
+import './Background.css'
+import video1 from '../../assets/video1.mp4'
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import image3 from '../../assets/image3.png'
 
-const background = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Background = ({ playStatus, heroCount }) => {
+  if (playStatus) {
+    return (
+      <video className='Background' autoPlay loop muted>
+        <source src={video1} type='video/mp4' />
+      </video>
+    )
+  } else if (heroCount === 0) {
+    return <img src={image1} className='Background' alt="" />
+  } else if (heroCount === 1) {
+    return <img src={image2} className='Background' alt="" />
+  } else if (heroCount === 2) {
+    return <img src={image3} className='Background' alt="" />
+  }
+
+  return null
 }
 
-export default background
+export default Background
